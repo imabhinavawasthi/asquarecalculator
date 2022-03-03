a706098600e6874de9362b3c82689cbaecb66efb
imabhinavawasthi <76579728+imabhinavawasthi@users.noreply.github.com>
2022-03-03 18:54:56 +0530

    Source Code

---
+++ app.js
@@ -0,0 +1,25 @@
+(function () {
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function (e) {
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    });
+
+    equal.addEventListener('click', function (e) {
+        if (screen.value === '') { screen.value = "Enter Value"; }
+        else {
+            let ans = eval(screen.value);
+            screen.value = ans.toPrecision(4);
+        }
+    });
+
+    clear.addEventListener('click', function (e) {
+        screen.value = '';
+    });
+})();
